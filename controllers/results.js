cd03fd8089e47c0d2f568c9987b3e3e113525d6d
riccardoferrara <riccardo85ferrara@gmail.com>
2022-01-25 06:43:29 +0000

    add results array

--- controllers/results.js
+++ controllers/results.js
@@ -3,9 +3,46 @@ const express = require('express')
 const app = require('../app')
 const router = express.Router()
 
+
+
 // Create POST controller
+results = [{
+    title: 'JS tutorials',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [{
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+    }, {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+    }]
+}, {
+    title: 'JS tutorials 2 test',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [{
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+    }, {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+    }]
+}, {
+    title: 'JS tutorials 3 test',
+    description: 'The best JavaScript tutorials in the galaxy!',
+    url: 'https://www.w3schools.com',
+    links: [{
+        title: 'JS for Beginners',
+        url: 'https://www.w3schools.com/js'
+    }, {
+        title: 'JS for the Web',
+        url: 'https://www.w3schools.com/js'
+    }]
+}]
+
+
 router.get('/', (req, res) => {
-        // res.send('This is the Results Page')
         res.render('results')
     })
     //
