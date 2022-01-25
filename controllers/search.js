b6eb94347e8658bb4410097633c2ce105b8a008e
riccardoferrara <riccardo85ferrara@gmail.com>
2022-01-25 06:32:35 +0000

    render search page thru .hbs

--- controllers/search.js
+++ controllers/search.js
@@ -6,7 +6,8 @@ const router = express.Router()
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
 router.get('/', (req, res) => {
-        res.send('This is the Search Page')
+        // res.send('This is the Search Page')
+        res.render('search')
     })
     // Export
 module.exports = router
