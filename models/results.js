9a60f7fc7f6d9a6f4149e8a423a985ab8e2cbcf9
riccardoferrara <riccardo85ferrara@gmail.com>
2022-01-27 08:11:59 +0000

    export the model results

--- models/results.js
+++ models/results.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 // Create the results moodel
-mongoose.model('results', {
+module.export = mongoose.model('results', {
     title: {
         type: String,
         required: true
